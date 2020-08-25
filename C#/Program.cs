7a474eaaf923b6b74dc1c967a78b6c772a68b070
Singlerr <33392666+Singlerr@users.noreply.github.com>
2020-08-26 08:48:33 +0900

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -9,8 +9,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
-            list.Add(new KeyValuePair<string, string>("score", "50"));
-            SendInfo.Send(list);
+            list.Add(new KeyValuePair<string, string>("<여기에 KEY를 넣으세요>", "<여기에 VALUE를 넣으세요>"));
+            SendInfo.Send(list); //정보를 서버에 전송
         }
     }
 }
