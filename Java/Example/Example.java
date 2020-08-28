c9fe361d99212f5ab77d0ed9b295e2c77454c8b6
Singlerr <33392666+Singlerr@users.noreply.github.com>
2020-08-29 00:08:55 +0900

    Update Example.java

--- Java/Example/Example.java
+++ Java/Example/Example.java
@@ -3,7 +3,7 @@ package kr.uchart;
 public class Example {
 
     public static void main(String[] args){
-        SendInfo task = new SendInfo("MyProgram");
+        SendInfo task = new SendInfo("your_email/MyProgram");
         task.addKeyValuePair("my key","my value");
         task.start();
     }
